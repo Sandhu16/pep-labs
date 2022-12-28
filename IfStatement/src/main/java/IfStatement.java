cb54834ada44e7cc73a55e0d8e4db8c8181a5791
Sandhu16 <sandhu11417@gmail.com>
2022-12-28 03:07:43 +0000

    Saved progress on IfStatement 2022-12-28 03:07:43 +0000

--- IfStatement/src/main/java/IfStatement.java
+++ IfStatement/src/main/java/IfStatement.java
@@ -10,13 +10,21 @@ public class IfStatement {
      * if bool is false, return y.
      *
      * Additional Resource if needed: https://www.w3schools.com/java/java_conditions.asp
-     *
-     * @param bool a true/false value that determines if x or y is to be returned.
-     * @param x first number to be compared.
-     * @param y second number to be compared.
-     * @return x if bool is true. if bool is false, return y.
-     */
-    public int ifExample(boolean bool, int x, int y){
-        return 0;
-    }
+    
+     
+        
+           
+    
+      
+      
+      
+        } 
+       
+       
+        
+
+
+
+    } 
+
 }
