23d243e984170fd575f8a1daa1f09cf14254acec
Sandhu16 <sandhu11417@gmail.com>
2023-01-01 20:30:35 +0000

    Saved progress on BooleanOperators 2023-01-01 20:30:35 +0000

--- BooleanOperators/src/main/java/Expressions.java
+++ BooleanOperators/src/main/java/Expressions.java
@@ -14,7 +14,7 @@ public class Expressions {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        return System.out.println(a^b);
+        return b;
         
         
     
@@ -26,7 +26,7 @@ public class Expressions {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        return b;
     }
 
     /**
