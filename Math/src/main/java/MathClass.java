9f911adabe9db7bc6a011e37926cc166c100eaa4
Sandhu16 <sandhu11417@gmail.com>
2023-01-07 23:47:08 +0000

    Saved progress on ForLoop 2023-01-07 23:47:08 +0000

--- Math/src/main/java/MathClass.java
+++ Math/src/main/java/MathClass.java
@@ -21,7 +21,7 @@ public class MathClass {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
-    return 
+    return 0;
         
     }
 }
